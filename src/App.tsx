0e807ce977bc93f0699599d06d948ad3b91cc823
Caio Anderson Ribeiro <69832122+CaioRibeiroDev@users.noreply.github.com>
2022-10-18 15:25:09 -0300

    :sparkles:feat(Master): create functionality transactions using context

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { ThemeProvider } from "styled-components";
+import { TransactionsProvider } from "./contexts/TransactionsContext";
 import { Transaction } from "./pages/Transactions";
 import { GlobalStyle } from "./styles/global";
 import { defaultTheme } from "./styles/themes/default";
@@ -9,7 +10,9 @@ export function App() {
     <ThemeProvider theme={defaultTheme}> 
       <GlobalStyle />
 
-      <Transaction />
+      <TransactionsProvider>
+        <Transaction />
+      </TransactionsProvider>
     </ThemeProvider>
   )
 }
