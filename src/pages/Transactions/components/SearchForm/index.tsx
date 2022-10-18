fc98cbb65344b79671a4872479894f32e48a2931
Caio Anderson Ribeiro <69832122+CaioRibeiroDev@users.noreply.github.com>
2022-10-18 17:14:47 -0300

    :sparkles:feat(Master): create functionality search with fetch

--- src/pages/Transactions/components/SearchForm/index.tsx
+++ src/pages/Transactions/components/SearchForm/index.tsx
@@ -1,7 +1,9 @@
 import { zodResolver } from '@hookform/resolvers/zod';
 import { MagnifyingGlass } from "phosphor-react";
+import { useContext } from 'react';
 import { useForm } from "react-hook-form";
 import * as z from 'zod';
+import { TransactionsContext } from '../../../../contexts/TransactionsContext';
 import { SearchFormContainer } from "./styles";
 
 const searchFormSchema = z.object({
@@ -11,14 +13,14 @@ const searchFormSchema = z.object({
 type SearchFormInputs = z.infer<typeof searchFormSchema>
 
 export function SearchForm() {
+  const { fetchTransactions } = useContext(TransactionsContext)
+
   const {register, handleSubmit, formState: { isSubmitting }} = useForm<SearchFormInputs>({
     resolver: zodResolver(searchFormSchema),
   })
   
   async function handleSearchTransactions(data: SearchFormInputs) {
-    // simulando uma demora de resposta
-    await new Promise(resolve => setTimeout(resolve, 2000))
-    console.log(data)
+    await fetchTransactions(data.query)
   }
 
   return (
