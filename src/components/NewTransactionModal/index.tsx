d6237750947864f7fc04f67fd675f17c89b96b64
Caio Anderson Ribeiro <69832122+CaioRibeiroDev@users.noreply.github.com>
2022-10-19 10:26:21 -0300

    :sparkles:feat(Master): add functionality create transaction

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,8 +1,10 @@
 import { zodResolver } from '@hookform/resolvers/zod';
 import * as Dialog from '@radix-ui/react-dialog';
 import { ArrowCircleDown, ArrowCircleUp, X } from 'phosphor-react';
+import { useContext } from 'react';
 import { Controller, useForm } from "react-hook-form";
 import * as z from 'zod';
+import { TransactionsContext } from '../../contexts/TransactionsContext';
 import { CloseButton, Content, Overlay, TransactionType, TransactionTypeButton } from './styles';
 
 const newTransactionFormSchema = z.object({
@@ -15,15 +17,23 @@ const newTransactionFormSchema = z.object({
 type NewTransactionFormInputs = z.infer<typeof newTransactionFormSchema>
 
 export function NewTransactionModal() {
-  const { register, handleSubmit, formState: {isSubmitting}, control} = useForm<NewTransactionFormInputs>({
+  const { createTransaction } = useContext(TransactionsContext)
+  
+  const { register, handleSubmit, formState: {isSubmitting}, control, reset} = useForm<NewTransactionFormInputs>({
     resolver: zodResolver(newTransactionFormSchema)
   })
 
   async function handleCreateNewTransaction(data: NewTransactionFormInputs) {
-    // simulando uma demora de resposta
-    await new Promise(resolve => setTimeout(resolve, 2000))
+    const { description, price, category, type } = data
     
-    console.log(data)
+    await createTransaction({
+      description, 
+      price, 
+      category, 
+      type
+    })
+
+    reset()
   }
   
   return (
